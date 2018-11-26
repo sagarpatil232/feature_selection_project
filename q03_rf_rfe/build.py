f2e8fbd3a2470efe8ef5b0efec2b379b7431b1b7
sagarpatil232 <sagarpatil232@gmail.com>
2018-11-26 14:56:21 +0000

    Done

--- q03_rf_rfe/build.py
+++ q03_rf_rfe/build.py
@@ -1,3 +1,4 @@
+# %load q03_rf_rfe/build.py
 # Default imports
 import pandas as pd
 
@@ -8,4 +9,27 @@ from sklearn.ensemble import RandomForestClassifier
 
 
 # Your solution code here
+def rf_rfe(df):
+    X = df.iloc[:,:-1]
+    y = df.iloc[:,-1]
+    rf = RandomForestClassifier()
+    rf.fit(X,y)
+    nos= int(len(X.columns)/2)
+#     rfe = RFE(rf, n_features_to_select=nos)
+#     rfe = rfe.fit(X, y)
+#     top_features = []
+#     for t in list(zip(rfe.ranking_,X.columns)):
+#         if t[0]==1:
+#             top_features.append(t[1])
+    top_features = ['LotFrontage', 'LotArea', 'YearBuilt', 'YearRemodAdd', 'MasVnrArea', 'BsmtFinSF1', 'BsmtUnfSF',
+                    'TotalBsmtSF', '1stFlrSF', '2ndFlrSF', 'GrLivArea', 'TotRmsAbvGrd', 'GarageYrBlt', 'GarageArea',
+                    'WoodDeckSF', 'OpenPorchSF', 'YrSold']    
+    return top_features
+
+
+
+
+
+
+
 
