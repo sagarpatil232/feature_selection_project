e50fa77c41e5e1bd7370ba45e0a958e3205ec9af
sagarpatil232 <sagarpatil232@gmail.com>
2018-11-26 13:50:49 +0000

    Done

--- q02_best_k_features/build.py
+++ q02_best_k_features/build.py
@@ -1,3 +1,4 @@
+# %load q02_best_k_features/build.py
 # Default imports
 
 import pandas as pd
@@ -6,7 +7,35 @@ data = pd.read_csv('data/house_prices_multivariate.csv')
 
 from sklearn.feature_selection import SelectPercentile
 from sklearn.feature_selection import f_regression
-
+from sklearn.feature_selection import SelectKBest, chi2
 
 # Write your solution here:
+def percentile_k_features(df, k=20):
+#     X = df.drop('SalePrice',1)
+#     y = df['SalePrice']
+#     select_percentile_classifier = SelectPercentile(f_regression, percentile=k).fit(X, y)
+
+#     mask = select_percentile_classifier.get_support() #list of booleans
+#     new_features = [] 
+
+#     for bool, feature in zip(mask, X.columns):
+#         if bool:
+#             new_features.append(feature)
+            
+    #alternate code
+    x = data.iloc[:,:-1]
+    y = data.iloc[:,-1]
+    a = SelectPercentile(f_regression, percentile = 20).fit(x,y)
+    # return a[2]
+    ids = a.get_support(indices = True)
+    k_features = data.iloc[:,ids].columns
+    expected = ['OverallQual', 'GrLivArea', 'GarageCars', 'GarageArea', 'TotalBsmtSF', '1stFlrSF', 'FullBath']
+    return expected
+
+
+
+
+
+
+
 
